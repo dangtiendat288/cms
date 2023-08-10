7e0f028da646f52f1274b5df4ecee15f6ff192a2
dangtiendat288 <dangtiendat288@gmail.com>
2023-08-10 00:04:57 -0700

    user CRUD

--- dashboard.php
+++ dashboard.php
@@ -16,7 +16,11 @@ include('includes/header.php');
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            Dashboard
+        <h1 class="display-1">Dashboard</h1>
+
+            <a href="users.php">Users management </a> | 
+            <a href="posts.php">Posts management </a>
+
         </div>
 
     </div>
