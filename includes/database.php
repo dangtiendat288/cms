a251e28e1cd2921087591dfbf2594a29fced8ae6
dangtiendat288 <dangtiendat288@gmail.com>
2023-08-15 12:50:57 -0700

    update db syntax

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,15 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'cms', 'cms', 'cms');
+$server = "localhost";
+// $username = "id21150522_cms";
+// $password = "Meomeomeo123@";
+// $dbname = "id21150522_cms";
+
+$username = "cms";
+$password = "cms";
+$dbname = "cms";
+
+$connect = mysqli_connect($server, $username, $password, $dbname);
 
 if (mysqli_connect_errno()){
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
